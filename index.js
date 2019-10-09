9bedb39243e344098dbf9db3f0917d0063995742
liofan1 <50534070+liofan1@users.noreply.github.com>
2019-10-09 11:35:30 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require ('discord.js');
+const pluralize = require('pluralize');
+
+const { AkairoClient } = require('discord-akairo');
+const client = new AkairoClient({
+    ownerID: '618080309342109749',
+    prefix: ';',
+    commandDirectory: './src/commands/',
+		inhibitorDirectory: './src/inhibitors/'
+});
+ 
+client.login('token').then(() => {
+    console.log('The bot is currently ONLINE.');
+});
